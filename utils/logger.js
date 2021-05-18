cfe1a1f001509eecd0789b35a2b9dcb0656acca3
Rowario <r0wario@ya.ru>
2021-05-18 20:39:02 +0300

    some fixes

--- utils/logger.js
+++
@@ -1,9 +0,0 @@
-const log4js = require('log4js');
-log4js.configure({
-    appenders: { error: { type: 'file', filename: 'error.log' } },
-    categories: { default: { appenders: ['error'], level: 'error' } }
-});
-const loggers = log4js.getLogger('error');
-loggers.level = 'ERROR';
-
-module.exports = loggers;
